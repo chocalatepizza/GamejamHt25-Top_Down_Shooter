914e0830fdb735e2b8064cad87b03e98611631c7
Nogard-Inmo <liam.nordlund09@gmail.com>
2025-11-04 09:16:21 +0100

    Player movement commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    //Movement
+    [SerializeField] KeyCode left = KeyCode.LeftArrow;
+    [SerializeField] KeyCode right = KeyCode.RightArrow;
+    [SerializeField] KeyCode up = KeyCode.UpArrow;
+    [SerializeField] KeyCode down = KeyCode.DownArrow;
+
+    [SerializeField, Range(1, 3)] float speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movement
+        if (Input.GetKey(left))
+        {
+            transform.position -= new Vector3(1, 0, 0) * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(right))
+        {
+            transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(up))
+        {
+            transform.position += new Vector3(0, 1, 0) * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(down))
+        {
+            transform.position -= new Vector3(0, 1, 0) * speed * Time.deltaTime;
+        }
+
+        //rotation
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        Vector2 dir = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+
+        transform.up = dir;
+
+
+
+
+    }
+}

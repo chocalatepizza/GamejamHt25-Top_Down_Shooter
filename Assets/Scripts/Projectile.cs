b9b78ed3d5f4881ba73ac824feb9ddd3b48b85bf
Nogard-Inmo <liam.nordlund09@gmail.com>
2025-11-04 11:08:08 +0100

    Projectile self delete fix

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
+using System.Collections;
 
 public class Projectile : MonoBehaviour
 {
 
     public float speed = 20f;
     public Rigidbody2D rb;
+    [SerializeField] float projectileDeleatSpeed = 1f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb.linearVelocity = transform.up * speed;
+        StartCoroutine(Deletee());
     }
 
     // Update is called once per frame
@@ -17,4 +20,11 @@ public class Projectile : MonoBehaviour
     {
         
     }
+
+    private IEnumerator Deletee()
+    {
+        yield return new WaitForSeconds(projectileDeleatSpeed);
+        Destroy(gameObject);
+    }
+
 }

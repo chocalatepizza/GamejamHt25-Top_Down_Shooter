a017b7ac745345391ee6935c65d1dd7a50e46198
GustawSiwert <Gustaw3.Westher@gmail.com>
2025-11-06 09:17:20 +0100

    player health code

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    public Slider healthBar;
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = currentHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int amount)
     {
-        
+        currentHealth -= amount;
+        if (currentHealth < 0)
+            currentHealth = 0;
+
+        healthBar.value = currentHealth;
+
+        if (currentHealth == 0)
+        {
+            Debug.Log("Player died!");
+            // Här kan du lägga till vad som händer när spelaren dör
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+        if (currentHealth > maxHealth)
+            currentHealth = maxHealth;
+
+        healthBar.value = currentHealth;
     }
 }

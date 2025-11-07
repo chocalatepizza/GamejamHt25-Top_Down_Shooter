ea851127208a77a6cef764a9dde9b124c4a122ba
chocalatepizza <charlielindenjohanson@gmail.com>
2025-11-07 11:04:31 +0100

    Shotgun sfx and music

--- Assets/enemyTank.cs
+++ Assets/enemyTank.cs
@@ -4,7 +4,8 @@ public class enemyTank : MonoBehaviour
 {
     private GameObject player;
     public float speed = 4f;
-    public float shootRange = 3f;
+    public float fireDistance = 3f;
+    public float shotCooldown = 1f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,19 +16,20 @@ public class enemyTank : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Finds the position of the player
+        //Finds the position of the player, looks towards it
         Vector2 playerPosition = player.transform.position;
         Vector2 direction = (playerPosition - (Vector2)transform.position).normalized;
         transform.up = direction;
 
-        //gets it to move
-        transform.position += transform.up * speed * Time.deltaTime;
-
         //checks distance between enemy and this enemy
         float playerDistance = Vector3.Distance(transform.position, playerPosition);
 
-        if (playerDistance < shootRange)
+        if (playerDistance < fireDistance)
         { speed = 0f; }
+        
+
         else { speed = 4f; }
+        transform.position += transform.up * speed * Time.deltaTime;
+
     }
 }
